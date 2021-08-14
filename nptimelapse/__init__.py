f2f140ac1d3ea8041694eef4679c098b1f7e9488
olus2000 <alsabak@gmail.com>
2021-08-14 18:11:42 +0100

    Simplest app test

---
+++ nptimelapse/__init__.py
@@ -0,0 +1,62 @@
+import os
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+##from nptimelapse import auth, index
+##from nptimelapse.model import
+##from nptimelapse.cli import 
+from nptimelapse.db import db
+
+
+def create_app(test_config=None):
+    global db
+    app = Flask(__name__)
+
+    # load config from envvars
+    app.config['SECRET_KEY'] = config_from_env('SECRET_KEY', 'zmien to')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = \
+        config_from_env('SQLALCHEMY_TRACK_MODIFICATIONS', False)
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        config_from_env('DATABASE_URL', False)
+
+    # load testing config if testing
+    if test_config is not None:
+        app.config.from_mapping(test_config)
+
+
+    # ensure the instance folder exists
+    try:
+        os.mkdir(app.instance_path)
+    except OSError:
+        pass
+
+    # connect to database
+    # SQLALCHEMY_DATABASE_URI
+    db.init_app(app)
+
+    # the simplest page
+    @app.route('/hello')
+    def hello():
+        return 'Henlo warld!'
+
+    # pages
+    # app.register_blueprint(auth.bp)
+
+    # model
+    # app.register_blueprint(user.bp)
+
+    # commandline arguments
+    # app.cli.add_command(init_db)
+
+    return app
+
+
+def config_from_env(variable, default=None):
+    ans = os.environ.get(variable)
+    if not ans:
+        return default
+    return ans
+
+
+app = create_app()
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+from nptimelapse import create_app
+
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run()
