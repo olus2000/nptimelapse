6e20080a761b0983e121e504f26f9fa1f98bfa11
olus2000 <alsabak@gmail.com>
2021-08-14 22:01:15 +0100

    Column names match python column names

--- nptimelapse/model/__init__.py
+++ nptimelapse/model/__init__.py
@@ -4,16 +4,16 @@ from nptimelapse.db import db
 class Game(db.Model):
     __tablename__ = 'game'
     id = db.Column('id', db.BigInteger(), primary_key=True)
-    api_key = db.Column('apikey', db.String(6), nullable=False)
+    api_key = db.Column('api_key', db.String(6), nullable=False)
     name = db.Column('name', db.String(40), nullable=False)
-    close_date = db.Column('closedate', db.DateTime())
+    close_date = db.Column('close_date', db.DateTime())
     stars = db.relationship('Star')
 
 
 class Star(db.Model):
     __tablename__ = 'star'
     id = db.Column('id', db.Integer(), primary_key=True)
-    game_id = db.Column('gameid', db.BigInteger(), db.ForeignKey('game.id'),
+    game_id = db.Column('game_id', db.BigInteger(), db.ForeignKey('game.id'),
                         primary_key=True)
     x = db.Column('x', db.Float(), nullable=False)
     y = db.Column('y', db.Float(), nullable=False)
@@ -23,8 +23,8 @@ class Star(db.Model):
 class Owner(db.Model):
     __tablename__ = 'owner'
     tick = db.Column('tick', db.Integer(), primary_key=True)
-    star_id = db.Column('starid', db.Integer(), primary_key=True)
-    game_id = db.Column('gameid', db.BigInteger(), db.ForeignKey('game.id'),
+    star_id = db.Column('star_id', db.Integer(), primary_key=True)
+    game_id = db.Column('game_id', db.BigInteger(), db.ForeignKey('game.id'),
                         primary_key = True)
     player = db.Column('player', db.SmallInteger(), nullable=False)
     __table_args__ = (
