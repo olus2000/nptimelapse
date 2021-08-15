03712d9cbbedfa3c76b83525e0065b00f0540702
olus2000 <alsabak@gmail.com>
2021-08-15 21:45:40 +0100

    Index

--- nptimelapse/index.py
+++ nptimelapse/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from sqlalchemy.sql import func
 
 from nptimelapse.db import db
@@ -12,5 +12,12 @@ bp = Blueprint('index', __name__, url_prefix='')
 def browse_games():
     # Query games
     games = db.session.query(func.min(Owner.tick), func.max(Owner.tick), Game) \
-        .join(Game.owners)
-    return str(games)
+        .join(Game.owners).group_by(Game.id).order_by(Game.id.desc()).all()
+
+    games = [{'start_tick': g[0],
+                'end_tick': g[1],
+                  'number': g[2],
+                    'name': g[4],
+              'close_date': g[5]}
+             for g in games]
+    return render_template('browse_games.html', games=games)
