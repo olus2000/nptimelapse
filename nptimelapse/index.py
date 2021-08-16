53d0389b8ebc4f434c8ac3cee8962ce5c55c0fd2
olus2000 <alsabak@gmail.com>
2021-08-16 11:36:33 +0100

    Game adding logic

--- nptimelapse/index.py
+++ nptimelapse/index.py
@@ -1,15 +1,65 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, flash
+from werkzeug.utils import redirect
 from sqlalchemy.sql import func
 
 from nptimelapse.db import db
-from nptimelapse.model import Game, Owner
+from nptimelapse.model import Game, Star, Owner
+
+import requests
 
 
 bp = Blueprint('index', __name__, url_prefix='')
 
 
-@bp.route('/')
+@bp.route('/', methods=('GET', 'POST'))
 def browse_games():
+    # A new game request
+    if request.method == 'POST':
+        game_id = request.form['game_id']
+        api_key = request.form['api_key']
+
+        # Check if game already exists
+        exists = Game.query.filter(Game.id == game_id).one_or_none()
+        if exists:
+            return redirect(url_for('index.game', game_id=game_id))
+
+        # Fetch game data from ironhelmet API
+        params = {'game_number': game_nr,
+                         'code': api_key,
+                  'api_version': '0.1'}
+        payload = requests.post('np.ironhelmet.com/api', params).json()
+
+        # Handle API errors
+        if 'error' in payload:
+            error = payload['error']
+            if error = 'code not found in game':
+                flash('Incorrect API key')
+            elif error = 'api_version not supported':
+                flash('API error. Contact the administartor')
+            else:
+                flash('Incorrect game number')
+        elif payload['scanning_data']['game_over']:
+            flash('Game has already ended')
+        else:
+            # Register the new game in DB
+            data = payload['scanning_data']
+            db.session.add(Game(id=game_id, api_key=api_key, name=data['name']))
+            new stars = [Star(id=int(star_id),
+                              game_id=game_id,
+                              x=float(star['x']),
+                              y=float(star['y']))
+                         for star_id, star in data['stars'].items()]
+            db.session.add_all(new_stars)
+            new_owners = [Owner(tick=data['tick'],
+                                star_id=int(star_id),
+                                game_id=game_id,
+                                player=star['puid'])
+                          for star_id, star in data['stars'].items() if star['puid'] >= 0]
+            db.session.add_all(new_owners)
+            return redirect(url_for('index.game', game_id=game_id))
+
+        # If an error happened the normal page is displayed
+
     # Query games
     games = db.session.query(func.min(Owner.tick), func.max(Owner.tick), Game) \
         .join(Game.owners).group_by(Game.id).order_by(Game.id.desc()).all()
@@ -22,7 +72,6 @@ def browse_games():
              for g in games]
     return render_template('browse_games.html', games=games)
 
-
-@bp.route('/new_game')
-def new_game():
-    return render_template('new_game.html')
+@bp.route('<int:game_id>/')
+def game(id):
+    return f'Game {game_id}'
