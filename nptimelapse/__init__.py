c7772a671fb36410cb00c4dc31a996a2001d167c
olus2000 <alsabak@gmail.com>
2021-08-16 17:50:05 +0100

    CLI registered in __init__

--- nptimelapse/__init__.py
+++ nptimelapse/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from nptimelapse import index
 ##from nptimelapse.model import
-from nptimelapse.cli import init_db
+from nptimelapse.cli import init_db, fetch_owners
 from nptimelapse.db import db
 
 
@@ -59,6 +59,7 @@ def create_app(test_config=None):
 
     # commandline arguments
     app.cli.add_command(init_db)
+    app.cli.add_command(fetch_owners)
 
     return app
 
