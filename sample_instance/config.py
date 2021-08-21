6546d695ca0b4788c773487a9920efca599079ea
Aleksander Sabak <alsabak@gmail.com>
2021-08-21 10:15:04 +0100

    Added celery

--- sample_instance/config.py
+++ sample_instance/config.py
@@ -2,3 +2,6 @@ SECRET_KEY = 'change this value'
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI = 'sqlite:////{instance}/nptimelapse.sqlite3'
+
+CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_BROKER_URL = 'amqp://localhost'
